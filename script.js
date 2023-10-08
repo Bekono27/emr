79f2eb306c2717a4686cc1c0580d64f08a507432
Bekono27 <137257995+Bekono27@users.noreply.github.com>
2023-10-09 00:08:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+.function triggerSOS() {
+    alert('SOS distress signal triggered: ··· – – – ···');
+  }
+  
+  function triggerCode(code) {
+    alert('Emergency code triggered: ' + code);
+  }
+  
